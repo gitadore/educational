52003fc8bf9cf4049d0acbcaabd3f55495d8a610
andi <andi@hoellenmaschine>
2014-08-29 23:45:55 +0200

    wandler interface hinzugefügt

--- GitTests/src/com/kaiserandi/Regelwerk.java
+++ GitTests/src/com/kaiserandi/Regelwerk.java
@@ -3,14 +3,24 @@ package com.kaiserandi;
 public class Regelwerk {
 
 	private String verb;
+	private IWandler wandler;
 
 	public Regelwerk(String verb) {
 		super();
 		this.verb = verb;
 	}
 	
+	public Regelwerk(String verb, IWandler wandler) {
+		super();
+		this.verb = verb;
+		this.wandler = wandler;
+	}
+	
 	public String getInfinitiv(){
+		if(wandler == null)
 			return new WandlerRegelmaessig().infinitiv(verb);
+		else
+			return wandler.infinitiv(verb);
 	}
 	
 }
